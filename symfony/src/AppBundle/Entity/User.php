6c2a1a802493f3378cd23b1e6d9c8045b7202fd9
Hélder Pestana <hgpestana@gmail.com>
2019-03-25 15:02:12 +0000

    Symfony first commit. Configured with FOSUserBundle to support authentication. App file set as DEV.

---
+++ symfony/src/AppBundle/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+
+// src/AppBundle/Entity/User.php
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="owl_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Column(type="guid")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="UUID")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
